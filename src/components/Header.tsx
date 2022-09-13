54cfbf93586f6580ae36461477caad574642a717
douglas-ssouza <douglasssouza1994@gmail.com>
2022-09-13 16:05:15 -0300

    feat: adds Icon to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,28 @@ import React from 'react';
 
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+import headerIcon from '../assets/icon_180.png';
 
 function Header() {
 	return (
-		<AppBar>
-			<Toolbar>
-				<Typography component='h1' variant='h5'>
-					Douglas Souza
-				</Typography>
-			</Toolbar>
-		</AppBar>
+		<>
+			<AppBar color='secondary'>
+				<Toolbar>
+					<Grid container>
+						<Grid item>
+							<img
+								src={headerIcon}
+								alt='Header icon with initials DS'
+								style={{padding: '.5em', width: '80px'}}
+							/>
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+			<Toolbar />
+		</>
 	);
 }
 
