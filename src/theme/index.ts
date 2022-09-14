c2eec87801576cdd9a4b24848dd56d71018b8c8b
douglas-ssouza <douglasssouza1994@gmail.com>
2022-09-14 09:37:37 -0300

    refactor: separates palette from theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,28 +1,9 @@
 import {createTheme} from '@mui/material/styles';
 
-const surfaceOrange = '#FF5E1E';
-const surfaceBrown = '#201F1B';
-const surfaceGrey = '#3F3C37';
-const textGrey = '#CCC6BA';
-const textWhite = '#FFFFFF';
+import palette from './palette';
 
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: surfaceOrange,
-		},
-		secondary: {
-			main: surfaceBrown,
-		},
-		text: {
-			primary: textWhite,
-			secondary: textGrey,
-		},
-		background: {
-			paper: surfaceGrey,
-			default: surfaceBrown,
-		},
-	},
+	palette,
 });
 
 export default theme;
---
+++ src/theme/palette.ts
@@ -0,0 +1,24 @@
+const surfaceOrange = '#FF5E1E';
+const surfaceBrown = '#201F1B';
+const surfaceGrey = '#3F3C37';
+const textGrey = '#CCC6BA';
+const textWhite = '#FFFFFF';
+
+const palette = {
+	primary: {
+		main: surfaceOrange,
+	},
+	secondary: {
+		main: surfaceBrown,
+	},
+	text: {
+		primary: textWhite,
+		secondary: textGrey,
+	},
+	background: {
+		paper: surfaceGrey,
+		default: surfaceBrown,
+	},
+};
+
+export default palette;
