b219831de27a98c110f4e9a569ed4bec543183a3
douglas-ssouza <douglasssouza1994@gmail.com>
2022-09-14 13:59:50 -0300

    feat: adds picture to Home page

---
+++ src/components/styled-components/ProfilePicture.tsx
@@ -0,0 +1,8 @@
+import {styled} from '@mui/material/styles';
+
+const ProfilePicture = styled('img')({
+	width: '20em',
+	borderRadius: '20%',
+});
+
+export default ProfilePicture;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import Grid from '@mui/material/Grid';
+
+import ProfilePicture from '../components/styled-components/ProfilePicture';
+
+import picture from '../assets/picture.jpg';
+
 function Home() {
 	return (
-		<h1>Home</h1>
+		<Grid container>
+			<Grid item>
+				<ProfilePicture src={picture} alt='Picture of Douglas Souza' />
+			</Grid>
+		</Grid>
 	);
 }
 
